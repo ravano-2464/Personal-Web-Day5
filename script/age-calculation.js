d91871fad7ea1171d993dc0e160900961982cc8a
Ravano Akbar Widodo <118270814+ravano-2464@users.noreply.github.com>
2024-08-21 10:01:16 +0700

    Bootcamp Personal Web Day 5

---
+++ script/age-calculation.js
@@ -0,0 +1,17 @@
+function calculateAge(birthDate) {
+    const today = new Date();
+    const birthDateObj = new Date(birthDate);
+    let age = today.getFullYear() - birthDateObj.getFullYear();
+    const monthDifference = today.getMonth() - birthDateObj.getMonth();
+    if (
+      monthDifference < 0 ||
+      (monthDifference === 0 && today.getDate() < birthDateObj.getDate())
+    ) {
+      age--;
+    }
+    return age;
+  }
+  
+  const birthDate = '2005-01-01'; 
+  const age = calculateAge(birthDate);
+  document.getElementById('age').textContent = age;  
